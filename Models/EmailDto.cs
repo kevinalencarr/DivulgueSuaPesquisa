49bea492a9c40cd9f58cae7d6e693e083a359eee
kevinalencarr <kevinribeiroalencar@gmail.com>
2023-11-28 17:16:11 -0300

    corrected project files organization

--- Models/EmailDto.cs
+++ Models/EmailDto.cs
@@ -4,29 +4,29 @@ namespace DivulgueSuaPesquisa.Models;
 
 public class EmailDto
 {
-    public string Subject { get; set; }
+    public string? Subject { get; set; }
     
     [Required]
-    public string Name { get; set; }
+    public string? Name { get; set; }
     
-    public string Lab { get; set; }
-    
-    [Required]
-    public string Dept { get; set; }
+    public string? Lab { get; set; }
     
     [Required]
-    public string ResearchTitle { get; set; }
+    public string? Dept { get; set; }
     
     [Required]
-    public string ResearchSummary { get; set; }
+    public string? ResearchTitle { get; set; }
     
     [Required]
-    public string PublicationLink { get; set; }
+    public string? ResearchSummary { get; set; }
     
     [Required]
-    public string ContactPhone { get; set; }
+    public string? PublicationLink { get; set; }
+    
+    [Required]
+    public string? ContactPhone { get; set; }
     
     [Required]
     [EmailAddress(ErrorMessage = "O formato do e-mail não é válido.")]
-    public string ContactEmail { get; set; }
+    public string? ContactEmail { get; set; }
 }
